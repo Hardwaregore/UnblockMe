776d6b7962f0cc189f4638cc919ad46639877dc0
Hardwaregore <hardwaregore101@gmail.com>
2022-07-06 14:49:01 -0500

    Create installClientside.sh

---
+++ InstallKasm/installClientside.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install chrome
+brew install --cask google-chrome
+
+echo "Please Download the Kasm chrome extention"
+echo "Installation Complete!"
